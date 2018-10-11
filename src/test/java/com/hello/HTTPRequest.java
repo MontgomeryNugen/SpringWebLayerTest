16a466e054a524808ebb7e7cf5043eecbf0a84fa
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-10-11 08:18:19 -0400

    initial commit

---
+++ src/test/java/com/hello/HTTPRequest.java
@@ -0,0 +1,30 @@
+package com.hello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HTTPRequest {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	TestRestTemplate restTemplate;
+	
+	@Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello World");
+    }
+
+}
